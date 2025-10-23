f1f19eb16f739b20ecfe0ac7035ace17f4fb6413
Korhan Ulusoy <korhanulusoy@bmc-mac-mww49ldyyy.fritz.box>
2025-10-23 17:08:08 +0200

    Fix: Update vite base config for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/Project-Area/", // BU SATIRIN DOĞRU OLDUĞUNDAN %100 EMİN OLUN
 })
